8d2a91dd374dca0d1bea8ca864ffdf35dd0c62b2
Mikhail Shprygov <mike.shp@hotmail.com>
2026-09-25 20:48:07 +0200

    Get proper user prompt and clear \n after fgets

--- main_logic.c
+++ main_logic.c
@@ -15,7 +15,20 @@ void main_logic()
 
 	printf("Enter a string: ");
 	fgets(string_1,sizeof(string_1),stdin);
-	// get rid of \n stored before \0
-	// avoid using strlen to achieve that
+	clear_newline(string_1);
 	printf("Your string is: %s\n",string_1);
+}
+void clear_newline(char *string)
+{
+	int c = 0;
+	while (string[c] != '\0' && string[c] != EOF)
+	{
+		if (string[c] == '\n')
+		{
+			string[c] = '\0';
+			break;
+		}
+
+		c++;
+	}
 }
--- main_logic.h
+++ main_logic.h
@@ -2,5 +2,6 @@
 #define MAIN_LOGIC_H
 
 void main_logic();
+void clear_newline(char*);
 
 #endif
